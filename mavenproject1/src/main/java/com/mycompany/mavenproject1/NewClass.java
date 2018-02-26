91689616016665a4fa508f08730e42206a6884eb
ic16b055 <ic16b055@technikum-wien.at>
2018-02-26 12:29:24 +0100

    NEW added

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -14,7 +14,7 @@ public class NewClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD NEW");
     }
     
 }
